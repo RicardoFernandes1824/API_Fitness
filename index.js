9bbbdc5aa553c86741caa095ed2e77218f007ea1
Ricardo Fernandes <ricardo.fernandes@ipvc.pt>
2025-07-23 01:09:52 +0100

    CORS added

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const {usersRouter} = require('./routes/users');
 const {accountRouter} = require('./routes/authentication');
 const {exerciseRouter} = require('./routes/exercise');
 const {workoutRoutineRouter} = require('./routes/workoutRoutine');
 const { workoutSessionRouter } = require('./routes/workoutSession');
 
+
 const verifyJWT = require('./middleware/verifyJWT');
 
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/uploads/profile_pics', express.static(path.join(__dirname, 'uploads/profile_pics')));
 
